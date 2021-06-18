163003eede8056af228352ed40194cc0c3318d4e
vkortikere <69329819+vkortikere@users.noreply.github.com>
2021-06-18 11:41:42 -0400

    Add files via upload

--- object-detection/main.js
+++ object-detection/main.js
@@ -8,8 +8,24 @@ function preload(){
 function setup(){
     canvas= createCanvas(640, 420);
     canvas.center();
-   
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+ 
 }
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.log(error);
+
+    }
+}
+
 
 function draw(){
 image(img, 0, 0, 640, 420);
---
+++ object-detection/zebra.js
@@ -0,0 +1,53 @@
+
+img="";
+objects = [];
+status="";
+
+function preload(){
+    img= loadImage('zebra.png');
+    }
+function setup(){
+    canvas= createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.log(error);
+
+    }
+}
+
+
+function draw(){
+image(img, 0, 0, 640, 420);
+if( status != ""){
+    for (i = 0; i < objects.length; i++){
+        document.getElementById("status").innerHTML = "Status : Object Detected";
+        fill("#FF0000");
+        percent = floor(objects[i].confidence *100);
+        text(objects[i].label+""+percent+"%", objects[i].x+ 15, objects[i].y+ 15);
+        noFill();
+        stroke("#FF0000");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+    }
+fill("#FF0000");
+text("Zebra", 75, 70);
+noFill();
+stroke("#FF0000");
+rect( 70, 50, 450, 350 );
+}
+
+function back(){
+    window.location = "index.html";
+}
